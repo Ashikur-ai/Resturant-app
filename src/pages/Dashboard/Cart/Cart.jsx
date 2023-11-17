7ddfea313ebdaf42ae824eb661f7004fb3588340
Ashikur-ai <ashikurrahman01755450127@gmail.com>
2023-11-17 18:38:41 +0600

    delete item implemented

--- src/pages/Dashboard/Cart/Cart.jsx
+++ src/pages/Dashboard/Cart/Cart.jsx
@@ -1,10 +1,43 @@
 import React from 'react';
 import useCart from '../../../hooks/useCart';
 import CartRow from './CartRow';
+import { FaRegTrashAlt } from "react-icons/fa";
+import Swal from 'sweetalert2';
+import useAxiosSecure from '../../../hooks/useAxiosSecure';
 
 const Cart = () => {
-    const [cart] = useCart();
+    const [cart, refetch] = useCart();
     const totalPrice = cart.reduce((total, item) => total + item.price, 0);
+    const axiosSecure = useAxiosSecure();
+
+    const handleDelete = id => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                
+                axiosSecure.delete(`/carts/${id}`)
+                    .then(res => {
+                        if (res.data.deletedCount > 0) {
+                            refetch();
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                        
+                })
+            }
+        });
+    }
     return (
         <div>
             <div className='flex justify-evenly'>
@@ -29,7 +62,41 @@ const Cart = () => {
                     <tbody>
                         {/* row 1 */}
                         {
-                            cart?.map(item => <CartRow key={item._id} item={item}></CartRow>)
+                            cart?.map((item, index) =>
+                                <tr key={item._id}>
+
+                                    <th>
+                                        <label>
+                                            {index+1}
+                                        </label>
+                                    </th>
+                                    <td>
+                                        <div className="flex items-center gap-3">
+                                            <div className="avatar">
+                                                <div className="mask mask-squircle w-12 h-12">
+                                                    <img src={item.image} alt="Avatar Tailwind CSS Component" />
+                                                </div>
+                                            </div>
+                                            <div>
+                                                <div className="font-bold"></div>
+
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        {item.name}
+                                    </td>
+                                    <td>{item.price}</td>
+                                    <th>
+                                        <button
+                                            onClick={()=>handleDelete(item._id)}
+                                            className="btn btn-ghost btn-xs">
+                                            <FaRegTrashAlt className='text-red-600'></FaRegTrashAlt>
+                                        </button>
+                                    </th>
+                                </tr> 
+
+                                )
                         }
                         
                     </tbody>
--- src/pages/Dashboard/Cart/CartRow.jsx
+++ src/pages/Dashboard/Cart/CartRow.jsx
@@ -1,38 +1,11 @@
 import React from 'react';
 
-const CartRow = (item) => {
-    console.log(item)
+const CartRow = (item, index) => {
+    console.log(index)
     const { name, image, price } = item.item;
     return (
         <>
-            <tr>
-                
-                <th>
-                    <label>
-                       
-                    </label>
-                </th>
-                <td>
-                    <div className="flex items-center gap-3">
-                        <div className="avatar">
-                            <div className="mask mask-squircle w-12 h-12">
-                                <img src={image} alt="Avatar Tailwind CSS Component" />
-                            </div>
-                        </div>
-                        <div>
-                            <div className="font-bold"></div>
-                            
-                        </div>
-                    </div>
-                </td>
-                <td>
-                    {name}
-                </td>
-                <td>{price}</td>
-                <th>
-                    <button className="btn btn-ghost btn-xs">details</button>
-                </th>
-            </tr>   
+              
         </>
     );
 };
