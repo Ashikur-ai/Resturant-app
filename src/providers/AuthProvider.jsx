ed8dd400fd164c6d4b9eff9141d60e74cbcfd9a7
Ashikur-ai <ashikurrahman01755450127@gmail.com>
2023-11-18 18:07:52 +0600

    checking admin and logout unauthorized access

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { createUserWithEmailAndPassword, updateCurrentUser, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { app } from "../firebase/firebase.config";
+import useAxiosPublic from "../hooks/useAxiosPublic";
  
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
@@ -8,6 +9,7 @@ const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
     const googleProvider = new GoogleAuthProvider();
+    const axiosPublic = useAxiosPublic();
 
     const createUser = (email, password) => {
         setLoading(true);
@@ -40,9 +42,17 @@ const AuthProvider = ({children}) => {
             setUser(currentUser);
             if (currentUser) {
                 // get token and store client 
+                const userInfo = { email: currentUser.email };
+                axiosPublic.post('/jwt', userInfo)
+                    .then(res => {
+                        if (res.data.token) {
+                            localStorage.setItem('access-token', res.data.token);
+                        }
+                })
             }
             else {
                 // do something
+                localStorage.removeItem('access-token');
             }
             setLoading(false);
         });
