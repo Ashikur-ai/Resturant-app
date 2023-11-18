bbd96fa88a2d27075f1e5183c6ebdf2139159624
Ashikur-ai <ashikurrahman01755450127@gmail.com>
2023-11-18 11:27:11 +0600

    saving user data while signup

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -4,8 +4,10 @@ import { useForm } from "react-hook-form";
 import { AuthContext } from "../../providers/AuthProvider";
 import { Link, useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
+import useAxiosPublic from "../../hooks/useAxiosPublic";
 
 const SignUp = () => {
+    const axiosPublic = useAxiosPublic();
     const {
         register,
         handleSubmit,
@@ -23,23 +25,34 @@ const SignUp = () => {
                 console.log(loggedUser);
                 updateUserProfile(data.name, data.photoURL)
                     .then(() => {
-                        console.log('user profile info updated')
-                        reset();
-                        Swal.fire({
-                            position: "top-end",
-                            icon: "success",
-                            title: "User created successfully",
-                            showConfirmButton: false,
-                            timer: 1500
-                        });
-                        navigate('/');
+                        const userInfo = {
+                            name: data.name,
+                            email: data.email
+                        }
+                        axiosPublic.post('/users', userInfo)
+                            .then(res => {
+                                if (res.data.insertedId) {
+                                    console.log('user added to the database')
+                                    reset();
+                                    Swal.fire({
+                                        position: "top-end",
+                                        icon: "success",
+                                        title: "User created successfully",
+                                        showConfirmButton: false,
+                                        timer: 1500
+                                    });
+                                    navigate('/');
+
+                                }
+                            })
+
                     })
-                .catch(error => console.log(error))
-        })
+                    .catch(error => console.log(error))
+            })
 
     };
 
-    
+
     return (
         <div className="hero min-h-screen bg-base-200">
             <Helmet>
@@ -56,21 +69,21 @@ const SignUp = () => {
                             <label className="label">
                                 <span className="label-text">Name</span>
                             </label>
-                            <input type="text" {...register("name", { required: true })} name="name" placeholder="email" className="input input-bordered"  />
+                            <input type="text" {...register("name", { required: true })} name="name" placeholder="Name" className="input input-bordered" />
                             {errors.name && <span className="text-red-600">This field is required</span>}
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Photo URL</span>
                             </label>
-                            <input type="text" {...register("photoURL", { required: true })}  placeholder="PhotoURL" className="input input-bordered"  />
+                            <input type="text" {...register("photoURL", { required: true })} placeholder="PhotoURL" className="input input-bordered" />
                             {errors.photoURL && <span className="text-red-600">This field is required</span>}
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="email" {...register("email", {required: true})} name="email" placeholder="email" className="input input-bordered" />
+                            <input type="email" {...register("email", { required: true })} name="email" placeholder="email" className="input input-bordered" />
                             {errors.email && <span className="text-red-600">This field is required</span>}
                         </div>
                         <div className="form-control">
@@ -83,16 +96,16 @@ const SignUp = () => {
                                 maxLength: 20,
                                 pattern: /(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z])/
                             })} placeholder="password" name="password" className="input input-bordered" />
-                            {errors.password?.type==='minLength'  && <p className="text-red-600">Password must be 6 characters long</p>}
-                            {errors.password?.type==='pattern'  && <p className="text-red-600">Password must 2 Uppercase, special character, 2 digits, 3 lowercase</p>}
-                            {errors.password?.type==='maxLength'  && <p className="text-red-600">Password must be less than 20 characters long</p>}
+                            {errors.password?.type === 'minLength' && <p className="text-red-600">Password must be 6 characters long</p>}
+                            {errors.password?.type === 'pattern' && <p className="text-red-600">Password must 2 Uppercase, special character, 2 digits, 3 lowercase</p>}
+                            {errors.password?.type === 'maxLength' && <p className="text-red-600">Password must be less than 20 characters long</p>}
                             <label className="label">
                                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                             </label>
                         </div>
                         <div className="form-control mt-6">
                             <input className="btn btn-primary" type="submit" value="Sign Up" />
-                            
+
                         </div>
                     </form>
                     <p>Already Have an Account? <Link to="/login">Login</Link></p>
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import { createContext, useEffect, useState } from "react";
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { createUserWithEmailAndPassword, updateCurrentUser, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile } from "firebase/auth";
 import { app } from "../firebase/firebase.config";
  
 export const AuthContext = createContext(null);
@@ -24,7 +24,7 @@ const AuthProvider = ({children}) => {
     }
 
     const updateUserProfile = (name, photo) => {
-        return updateUserProfile(auth.currentUser, {
+        return updateProfile(auth.currentUser, {
             displayName: name, photoURL: photo
         });
     }
@@ -47,6 +47,7 @@ const AuthProvider = ({children}) => {
         signIn,
         logOut,
         updateUserProfile
+        
     }
     return (
         <AuthContext.Provider value={authInfo}>
